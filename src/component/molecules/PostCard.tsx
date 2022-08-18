d6dd21501abe9b5cf2f8851ae54664a01227e223
atamakonkurii <morisudesu@gmail.com>
2022-08-18 23:00:25 +0900

    PC版のカードは完成

--- src/component/molecules/PostCard.tsx
+++ src/component/molecules/PostCard.tsx
@@ -15,33 +15,39 @@ export const PostCard = (props: Props) => {
     props;
   return (
     <Link href={`/post/${id}`}>
-      <div className=" bg-slate-200 rounded-lg shadow-md">
-        <div className="flex h-60">
-          <div className="pl-6 w-96 h-48">
+      <div className="max-w-3xl bg-white rounded-lg shadow-md">
+        <div className="flex ml-4">
+          <div className="relative w-96 h-72">
             <Image
               src={postPhotoUrl}
-              width={900}
-              height={600}
+              layout="fill"
               objectFit="contain"
               alt="post"
             />
           </div>
-          <div className="m-4">
-            <div className="">{content}</div>
-            <div className="flex items-center">
-              <div className="w-12">
-                <Image
-                  src={publisherPhotoUrl}
-                  width={30}
-                  height={30}
-                  layout="responsive"
-                  alt="publisher"
-                  className="rounded-full"
-                />
+          <div className="relative my-8 mx-4 w-96">
+            <div className="font-semibold tracking-tight whitespace-pre-line line-clamp-6">
+              {content}
+            </div>
+            <div className="absolute bottom-0 left-0">
+              <div className="flex items-center">
+                <div className="w-12">
+                  <Image
+                    src={publisherPhotoUrl}
+                    width={30}
+                    height={30}
+                    layout="responsive"
+                    alt="publisher"
+                    className="rounded-full"
+                  />
+                </div>
+                <div className="pl-2 text-sm font-medium text-slate-600">
+                  {date}
+                </div>
+                <div className="pl-2 text-sm font-medium text-slate-600">
+                  ♡{favoriteCount}
+                </div>
               </div>
-
-              <div className="pl-2 text-sm font-semibold">{date}</div>
-              <div className="pl-2 text-sm font-semibold">{favoriteCount}</div>
             </div>
           </div>
         </div>
