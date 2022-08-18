be5eb67187d139759b12ab6e1d8d5ff8d2fbb941
atamakonkurii <morisudesu@gmail.com>
2022-08-19 07:22:04 +0900

    不要なbasetextを削除

--- src/constants/baseText.ts
+++ src/constants/baseText.ts
@@ -1,8 +1,4 @@
 /* eslint-disable @typescript-eslint/naming-convention */
-// FirstView
-export const BaseFirstViewSmallText =
-  "おごられたいを前面に出したポートフォリオサイト";
-export const BaseCTAButtonText = "とりあえずおごる！";
 
 // News
 export const BaseNewsDate = "2022-06-29T12:40:06.805Z";
@@ -17,21 +13,3 @@ export const BasePhotoUrlForPostCard = "/img/exp_fujisan.webp";
 export const BasePublisherPhotoUrlForPostCard = "/img/face.webp";
 export const BaseDateForPostCard = "2022/06/29";
 export const BaseFavoriteCountForPostCard = 100;
-
-// AboutSite
-export const BaseAboutSiteTitle = "このサイトについて";
-export const AboutSiteButtonText = "もう少し詳しく";
-export const AboutSiteButtonLink = "/no_page";
-
-// AboutAuthor
-export const BaseAboutAuthorTitle = "作者について";
-export const AboutAuthorButtonText = "もう少し詳しく";
-export const AboutAuthorButtonLink = "/no_page";
-
-// Portfolio
-export const BasePortfolioTitle = "今までの作品";
-export const PortfolioButtonText = "もっとみる";
-export const PortfolioButtonLink = "https://github.com/atamakonkurii";
-
-// BuyMeThings
-export const BaseBuyMeThingsTitle = "おごりプラン";
