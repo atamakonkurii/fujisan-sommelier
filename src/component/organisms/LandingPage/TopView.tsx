48e9b56daac4ccc3a7c50cb4a5a47185a2eb8f3f
atamakonkurii <morisudesu@gmail.com>
2022-08-19 08:09:13 +0900

    トップビューのコンポーネントを追加

---
+++ src/component/organisms/LandingPage/TopView.tsx
@@ -0,0 +1,17 @@
+import type { FC } from "react";
+
+export const TopView: FC = () => {
+  return (
+    <div className="text-white bg-gradient-to-r from-sky-500 to-indigo-500">
+      <div className="pt-10 text-xs text-center sm:text-xl">
+        富士山のフリー素材なら
+      </div>
+      <div className="mt-3 text-3xl font-bold text-center sm:mt-8 sm:text-6xl">
+        富士山ソムリエ
+      </div>
+      <div className="pb-10 pl-32 text-xs text-center sm:pl-52">
+        fujisan-smmelier
+      </div>
+    </div>
+  );
+};
