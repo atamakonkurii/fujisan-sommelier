b56910679fb3955eb1f13dd0e316c5d1275b2d12
atamakonkurii <morisudesu@gmail.com>
2022-08-25 08:37:36 +0900

    apiの作成

---
+++ pages/api/posts/index.ts
@@ -0,0 +1,14 @@
+import type { Post } from "@prisma/client";
+import { PrismaClient } from "@prisma/client";
+import type { NextApiRequest, NextApiResponse } from "next";
+
+const prisma = new PrismaClient();
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse<Post[]>
+) {
+  /* 投稿リストを取得 */
+  const posts = await prisma.post.findMany();
+  res.status(200).json(posts);
+}
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,15 @@
 import type { CustomNextPage } from "next";
+import { useEffect } from "react";
 import { StandardLayout } from "src/component/layout/StandardLayout";
 import { Index } from "src/pages/index";
 
 const IndexPage: CustomNextPage = () => {
+  useEffect(() => {
+    fetch("/api/posts").then((res) => {
+      return res.json();
+    });
+    // .then(console.log);
+  }, []);
   return <Index />;
 };
 
