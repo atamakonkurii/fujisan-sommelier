115c3a34a6d3d58437fdcab70bc1138b2ab7f3bc
atamakonkurii <morisudesu@gmail.com>
2022-08-26 23:09:25 +0900

    一旦追加

--- pages/posts/[id].tsx
+++ pages/posts/[id].tsx
@@ -1,12 +1,15 @@
 import type { CustomNextPage } from "next";
 import type { GetStaticProps } from "next";
+import { useRouter } from "next/router";
 import { StandardLayout } from "src/component/layout/StandardLayout";
 import { ShowPosts } from "src/pages/posts/[id]";
 import type { PostShowPageType } from "src/type/types";
 
 export const getStaticPaths = async () => {
-  const paths: string[] = [];
-  return { paths, fallback: false };
+  return {
+    paths: [],
+    fallback: "blocking",
+  };
 };
 
 const ShowPostsPage: CustomNextPage<PostShowPageType> = (post) => {
@@ -18,7 +21,10 @@ ShowPostsPage.getLayout = StandardLayout;
 export default ShowPostsPage;
 
 export const getStaticProps: GetStaticProps = async () => {
-  const res = await fetch(`${process.env.WEBAPP_URL}/api/posts/1`);
+  // eslint-disable-next-line react-hooks/rules-of-hooks
+  const router = useRouter();
+  const { id } = router.query;
+  const res = await fetch(`${process.env.WEBAPP_URL}/api/posts/${id}`);
   const post = await res.json();
   return {
     props: {
