35ffc1566e9bec711be346fecc6d19a1dbb346e6
atamakonkurii <morisudesu@gmail.com>
2022-08-28 23:09:10 +0900

    newsのコンポーネントを追加

---
+++ src/component/templates/News/IndexNewsContailner.stories.tsx
@@ -0,0 +1,40 @@
+import type { ComponentMeta, ComponentStory } from "@storybook/react";
+// eslint-disable-next-line no-restricted-imports
+import React from "react";
+import {
+  BaseNewsDate,
+  BaseNewsTitle,
+  BaseNewsType,
+} from "src/constants/baseText";
+import type { NewsArrayResponse } from "src/type/types";
+
+import { IndexNewsContainer } from "./IndexNewsContainer";
+
+// eslint-disable-next-line import/no-default-export
+export default {
+  title: "Templates/News/IndexNewsContainer ",
+  component: IndexNewsContainer,
+} as ComponentMeta<typeof IndexNewsContainer>;
+
+const newsResponse: NewsArrayResponse = {
+  news: [
+    {
+      id: "1",
+      title: BaseNewsTitle,
+      publishedAt: BaseNewsDate,
+      category: { name: BaseNewsType },
+    },
+    {
+      id: "2",
+      title: BaseNewsTitle,
+      publishedAt: BaseNewsDate,
+      category: { name: BaseNewsType },
+    },
+  ],
+};
+
+const Template: ComponentStory<typeof IndexNewsContainer> = () => {
+  return <IndexNewsContainer {...newsResponse} />;
+};
+
+export const Normal = Template.bind({});
