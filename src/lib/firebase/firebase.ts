e06e074b8efc6f6c6547a6b86830b7e22b8de341
atamakonkurii <morisudesu@gmail.com>
2022-09-01 08:35:56 +0900

    環境変数を追加

--- src/lib/firebase/firebase.ts
+++ src/lib/firebase/firebase.ts
@@ -7,13 +7,13 @@ import { initializeApp } from "firebase/app";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyAKvaBpypk218cXdFFaajEBjfpxRh8NXO4",
-  authDomain: "fujisan-sommelier-auth.firebaseapp.com",
-  projectId: "fujisan-sommelier-auth",
-  storageBucket: "fujisan-sommelier-auth.appspot.com",
-  messagingSenderId: "73675267897",
-  appId: "1:73675267897:web:0840c3e08c085819b892e8",
-  measurementId: "G-9LQHH2FKH0",
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGE_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID,
 };
 
 // Initialize Firebase
